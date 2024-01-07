ab3a25b25f496a47b7011700199a6b582404e830
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-07 23:21:46 +0800

    Update temporary main entry file and CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.jsx'
+import './index.css';
+import FormButton from './FormButton.jsx';
+import ProgressBar from './ProgressBar.jsx';
+import FormInput from './FormInput.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FormInput />
+    <ProgressBar />
+    <FormButton variant="solid" />
   </React.StrictMode>,
 )
