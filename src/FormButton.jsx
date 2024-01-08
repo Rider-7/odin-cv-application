67b08b2ae058cfa4b0f70925fb6a9eb363ef5170
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-08 14:49:16 +0800

    Update FormButton

--- src/FormButton.jsx
+++ src/FormButton.jsx
@@ -1,6 +1,6 @@
 import './FormButton.css';
 
-export default function FormButton({ value = '{value}', onClick, variant }) {
+export default function FormButton({ value, onClick, text = "Button", variant }) {
   let className = 'form-button';
   if (variant === 'solid') className += ' form-button--variant-solid';
 
@@ -9,8 +9,9 @@ export default function FormButton({ value = '{value}', onClick, variant }) {
       className={className}
       type="button"
       onClick={onClick}
+      value={value}
     >
-      {value}
+      {text}
     </button>
   );
 }
