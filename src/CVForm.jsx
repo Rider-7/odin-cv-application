f1c9bde417fe793b749ee080c672710bbd59fcb2
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-08 14:52:57 +0800

    Create CVForm React Component (WIP)

---
+++ src/CVForm.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import ProgressBar from './ProgressBar';
+import FormInput from './FormInput';
+import FormButton from './FormButton';
+import './CVForm.css';
+
+export default function CVForm() {
+  const steps = 4;
+  const [currentStep, setCurrentStep] = useState(1);
+
+  function onClickDelegator(e) {
+    switch (e.target.value) {
+      case 'next':
+        if (currentStep < steps) setCurrentStep(currentStep + 1);
+        break;
+      case 'prev':
+        if (currentStep > 1) setCurrentStep(currentStep - 1);
+        break;
+      default:
+        throw new Error('navController error!');
+    }
+  }
+
+  return (
+    <div className="cv-form">
+      <h1 className="cv-form__header">General Information</h1>
+      <ProgressBar steps={steps} currentStep={currentStep} />
+      <Section currentStep={currentStep} />
+      <Navigation onClickDelegator={onClickDelegator} />
+    </div>
+  );
+}
+
+// TODO: Make this component dynamic.
+function Section({ currentStep }) {
+  switch (currentStep) {
+    case 1:
+      return (
+        <div className="cv-form__section">
+          <FormInput title="First Name" type="text" />
+          <FormInput title="Last Name" type="text" />
+          <FormInput title="Email" type="email" />
+          <FormInput title="Phone Number" type="text" inputMode="numeric" />
+        </div>
+      );
+    case 2:
+      return (
+        <div className="cv-form__section">
+          <FormInput title="University" type="text" />
+          <FormInput title="Major" type="text" />
+          <FormInput title="Graduation Date" type="date" />
+          <FormInput title="GPA" type="text" inputMode='numeric' />
+        </div>
+      );
+    default:
+      return <span>ERROR</span>;
+  }
+}
+
+function Navigation({ onClickDelegator }) {
+  return (
+    <div className="cv-form__nav-main">
+      <div className="cv-form__nav-left">
+        <FormButton value="save_exit" text="Save & Exit" onClick={onClickDelegator} />
+      </div>
+      <div className="cv-form__nav-right">
+        <FormButton value="prev" text="Previous" onClick={onClickDelegator} />
+        <FormButton value="next" text="Next" onClick={onClickDelegator} />
+      </div>
+    </div>
+  );
+}
