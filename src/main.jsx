e5125809359f74a0c3334c5da5790082fdb87b89
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-08 14:52:21 +0800

    Prepare entry file for CVForm

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App.jsx'
 import './index.css';
-import FormButton from './FormButton.jsx';
-import ProgressBar from './ProgressBar.jsx';
-import FormInput from './FormInput.jsx';
+import CVForm from './CVForm';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormInput />
-    <ProgressBar />
-    <FormButton variant="solid" />
+    <CVForm />
   </React.StrictMode>,
 )
